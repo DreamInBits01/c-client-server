b1eebe1f2fb1e6e0ddaf66e65ccf56d94fa94e52
dreaminbits01 <mohammed.knite25@gmail.com>
2026-01-06 01:44:43 +0300

    Refactor client and server code for improved socket communication and error handling

--- client.c
+++ client.c
@@ -4,6 +4,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <string.h>
+#define RESPONSE_SIZE 256
+#define PORT 4002
+
 /*
 
 
@@ -16,8 +20,14 @@
 
 
 */
-int main()
+int main(int ac, char **argv)
 {
+    if (ac != 2)
+    {
+        printf("Please Provide the correct number of args\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("--- CLIENT ---\n");
     // Create the socket (stream of bytes exposed over the network)
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_fd < 0)
@@ -30,7 +40,7 @@ int main()
     // AF_INET = TCP server
     server_address.sin_family = AF_INET;
     // Port to connect to
-    server_address.sin_port = htons(3000);
+    server_address.sin_port = htons(PORT);
     //
     server_address.sin_addr.s_addr = INADDR_ANY;
 
@@ -40,9 +50,45 @@ int main()
         perror("Error connection failure\n");
         exit(EXIT_FAILURE);
     }
-    char server_response[256];
-    recv(socket_fd, server_response, sizeof(server_response), 0);
-    printf("Received data:%s\n", server_response);
+    int message_length = strlen(argv[1]);
+    int bytes_sent = send(socket_fd, argv[1], message_length, 0);
+    if (bytes_sent == -1)
+    {
+        printf("Error while sending message\n");
+    }
+    else if (bytes_sent == message_length)
+    {
+        printf("Message was sent completely\n");
+    }
+    else
+    {
+        printf("Message was sent partially (%d bytes)\n", bytes_sent);
+    }
+    int bytes_read = 1;
+    char server_response[RESPONSE_SIZE];
+    // Wait for a response via while
+    while (bytes_read >= 0)
+    {
+        bytes_read = recv(socket_fd, server_response, RESPONSE_SIZE, 0);
+        if (bytes_read == 0)
+        {
+            printf("Server closed connection\n");
+            break;
+        }
+        else if (bytes_read == -1)
+        {
+            printf("Error while receiving bytes\n");
+            break;
+        }
+        else
+        {
+            // Message got
+            server_response[bytes_read] = '\0';
+            printf("Received data:%s\n", server_response);
+            break;
+        }
+    }
+    printf("Closing socket\n");
     close(socket_fd);
     return 0;
 }
--- server.c
+++ server.c
@@ -1,37 +1,88 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <string.h>
-int main()
+#define REQUEST_SIZE 1024
+#define RESPONSE_SIZE 1024
+#define PORT 4002
+/*
+Some computer architectures use big endian or little endain, however networks are configured to use big endian.
+Bytes sent from the host to the network must be converted to big endian first, then should be transmitted over
+Bytes received from the network must be converted back to the machine's byte ordering
+
+uint32_t htonl(uint32_t hostlong);  //"Host to network long"
+uint16_t htons(uint16_t hostshort); //"Host to network short"
+uint32_t ntohl(uint32_t netlong);   //"Network to host long"
+uint16_t ntohs(uint16_t netshort);  //"Network to host short"
+*/
+typedef struct sockaddr SocketAddress;
+void check(int value, char *error_message)
 {
-    // Create the socket (stream of bytes exposed over the network)
-    char server_response[256] = "You have connected to the server\r\n";
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_fd < 0)
+    if (value < 0)
     {
-        perror("Error while opening socket\n");
+        printf("%s", error_message);
         exit(EXIT_FAILURE);
     }
-    struct sockaddr_in socket_address;
-    socket_address.sin_family = AF_INET;
-    socket_address.sin_port = htons(3000);
-    socket_address.sin_addr.s_addr = INADDR_ANY;
-    // We bind the socket to an address instead of connecting to it
-    bind(socket_fd, (struct sockaddr *)&socket_address, sizeof(socket_address));
+}
+int create_server_socket(int port)
+{
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    check(socket_fd, "Error while opening a socket\n");
+    struct sockaddr_in server_address;
+    // Zero out structs
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_port = htons(PORT);
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    // Bind the socket to an address
+    check(
+        bind(socket_fd, (SocketAddress *)&server_address, sizeof(server_address)),
+        "Bind faild\n");
+    check(
+        listen(socket_fd, 1),
+        "Listen failed\n");
+    printf("Listening on port 3000\n");
+    return socket_fd;
+}
+int accept_client(int server_fd)
+{
+}
+int main()
+{
+    printf("--- Server ---\n");
+    int socket_fd = create_server_socket(PORT);
+    char response[RESPONSE_SIZE] = "HTTP/1.1 200 OK\r\n\n";
+    char request[REQUEST_SIZE];
+    struct sockaddr_storage client_address;
+    socklen_t client_address_size;
+    memset(&client_address, 0, sizeof(client_address));
 
-    // Listen to that address
-    listen(socket_fd, 1);
-
-    // Accept clients
     while (1)
     {
-        struct sockaddr client_address;
-        int client_fd = accept(socket_fd, &client_address, NULL);
-        send(client_fd, server_response, sizeof(server_response), 0);
+        int client_fd = accept(socket_fd, (SocketAddress *)&client_address, &client_address_size);
+        memset(request, 0, REQUEST_SIZE);
+        int bytes_received = recv(client_fd, request, REQUEST_SIZE, 0);
+        if (bytes_received == 0)
+        {
+            printf("Client closed connection\n");
+            exit(EXIT_FAILURE);
+        }
+        else if (bytes_received == -1)
+        {
+            printf("Error while recieving data\n");
+            exit(EXIT_FAILURE);
+        }
+        request[bytes_received] = '\0';
+        printf("Request body:%s\n", request);
+        send(client_fd, response, RESPONSE_SIZE, 0);
+        close(client_fd);
     }
+    check(
+        close(socket_fd), "Error while closing server\n");
+    printf("Server closed\n");
     close(socket_fd);
-    return 0;
+    return EXIT_SUCCESS;
 }
