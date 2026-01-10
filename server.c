46161442b677f5e756da4de4771a581f249e5a83
dreaminbits01 <mohammed.knite25@gmail.com>
2026-01-10 03:24:26 +0300

    refactor client handling

--- server.c
+++ server.c
@@ -13,6 +13,12 @@ Some computer architectures use big endian or little endain, however networks ar
 Bytes sent from the host to the network must be converted to big endian first, then should be transmitted over
 Bytes received from the network must be converted back to the machine's byte ordering
 
+
+When two processes wish to communicate,
+the sending socket (client) must know the server's ip and the open socket on that machine,
+thus a number is needed to asign to that socket.
+
+
 uint32_t htonl(uint32_t hostlong);  //"Host to network long"
 uint16_t htons(uint16_t hostshort); //"Host to network short"
 uint32_t ntohl(uint32_t netlong);   //"Network to host long"
@@ -47,6 +53,28 @@ int create_server_socket(int port)
     printf("Listening on port 3000\n");
     return socket_fd;
 }
+int handle_client(int client_fd)
+{
+    char response[RESPONSE_SIZE] = "HTTP/1.1 200 OK\r\n\n";
+
+    char request[REQUEST_SIZE];
+    memset(request, 0, sizeof(request));
+    int bytes_received = recv(client_fd, request, REQUEST_SIZE, 0);
+    if (bytes_received == 0)
+    {
+        printf("Client closed connection\n");
+        exit(EXIT_FAILURE);
+    }
+    else if (bytes_received == -1)
+    {
+        printf("Error while recieving data\n");
+        exit(EXIT_FAILURE);
+    };
+    request[bytes_received] = '\0';
+    printf("Request body:%s\n", request);
+    send(client_fd, response, RESPONSE_SIZE, 0);
+    close(client_fd);
+}
 int accept_client(int server_fd)
 {
 }
@@ -54,8 +82,6 @@ int main()
 {
     printf("--- Server ---\n");
     int socket_fd = create_server_socket(PORT);
-    char response[RESPONSE_SIZE] = "HTTP/1.1 200 OK\r\n\n";
-    char request[REQUEST_SIZE];
     struct sockaddr_storage client_address;
     socklen_t client_address_size;
     memset(&client_address, 0, sizeof(client_address));
@@ -63,22 +89,7 @@ int main()
     while (1)
     {
         int client_fd = accept(socket_fd, (SocketAddress *)&client_address, &client_address_size);
-        memset(request, 0, REQUEST_SIZE);
-        int bytes_received = recv(client_fd, request, REQUEST_SIZE, 0);
-        if (bytes_received == 0)
-        {
-            printf("Client closed connection\n");
-            exit(EXIT_FAILURE);
-        }
-        else if (bytes_received == -1)
-        {
-            printf("Error while recieving data\n");
-            exit(EXIT_FAILURE);
-        }
-        request[bytes_received] = '\0';
-        printf("Request body:%s\n", request);
-        send(client_fd, response, RESPONSE_SIZE, 0);
-        close(client_fd);
+        handle_client(client_fd);
     }
     check(
         close(socket_fd), "Error while closing server\n");
